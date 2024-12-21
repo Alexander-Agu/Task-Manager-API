8a483d55578e1b954c5166d36e60d9e3ccdc3053
Alexander-Agu <theonlyagu@gmail.com>
2024-12-21 16:30:31 +0200

    Created the entity, controller, servoce & repository packages

---
+++ task-manager/src/main/java/com/theonlyagu/task_manager/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.theonlyagu.task_manager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "taskmanager")
+public class UserController {
+}
