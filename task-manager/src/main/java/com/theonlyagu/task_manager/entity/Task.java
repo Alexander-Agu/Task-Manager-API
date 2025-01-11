f7a113f1eea9d0efb31215bbbe9063cd3f80ea07
Alexander-Agu <theonlyagu@gmail.com>
2025-01-11 17:39:37 +0200

    Added more columns in the task Table

--- task-manager/src/main/java/com/theonlyagu/task_manager/entity/Task.java
+++ task-manager/src/main/java/com/theonlyagu/task_manager/entity/Task.java
@@ -1,13 +1,16 @@
 package com.theonlyagu.task_manager.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taskId_sequence")
-    @SequenceGenerator(name = "taskid_sequence", sequenceName = "taskId_sequence", allocationSize = 1)
+    @SequenceGenerator(name = "taskId_sequence", sequenceName = "taskId_sequence", allocationSize = 1)
     @Column(name = "task_id")
     private Long taskId;
 
@@ -18,6 +21,30 @@ public class Task {
     @Column(nullable = false)
     private String task;
 
+    @CreationTimestamp
+    @Column(updatable = false, name = "date_created", nullable = false)
+    private LocalDate dateCreated;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean complete;
+
+    public Task(boolean complete, LocalDate dueDate, LocalDate dateCreated, String task) {
+        this.complete = complete;
+        this.dueDate = dueDate;
+        this.dateCreated = dateCreated;
+        this.task = task;
+    }
+
+    public Task() {
+    }
+
+    public Task(String task) {
+        this.task = task;
+    }
+
     public Long getTaskId() {
         return taskId;
     }
@@ -26,11 +53,8 @@ public class Task {
         this.taskId = taskId;
     }
 
-    public Task() {
-    }
-
-    public Task(String task) {
-        this.task = task;
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public String getTask() {
@@ -41,11 +65,31 @@ public class Task {
         this.task = task;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public Long getUser() {
+        return user.getId();
     }
 
-    public User getUser() {
-        return user;
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
     }
 }
